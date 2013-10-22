ce13529f6098202af962b9eab7d585d13b1480ba
Wu Yongzheng <wuyongzheng@gmail.com>
2013-10-22 11:59:12 +0800

    no img

--- instat.cpp
+++ instat.cpp
@@ -14,13 +14,9 @@ struct insrecord {
 	ADDRINT high;
 };
 
-const char *imgname = "ld-linux.so.2";
 const char *logname = "instat.log";
 const char *tsvname = "instat.tsv";
 FILE *log;
-ADDRINT imglow = 0;
-ADDRINT imghigh = 0;
-bool imgloaded = false;
 std::map<ADDRINT,insrecord> insmap;
 
 void img_load (IMG img, void *v)
@@ -29,20 +25,6 @@ void img_load (IMG img, void *v)
 			IMG_Name(img).c_str(),
 			IMG_LoadOffset(img), IMG_LowAddress(img), IMG_HighAddress(img),
 			IMG_StartAddress(img), IMG_SizeMapped(img));
-	if (IMG_Name(img).rfind(imgname) != std::string::npos) {
-		fprintf(log, "matched\n");
-		imglow = IMG_LowAddress(img);
-		imghigh = IMG_HighAddress(img);
-		imgloaded = true;
-	}
-}
-
-void img_unload (IMG img, void *v)
-{
-	if (IMG_Name(img).rfind(imgname) != std::string::npos) {
-		fprintf(log, "unload matched\n");
-		imgloaded = false;
-	}
 }
 
 void on_ins (ADDRINT insaddr, struct insrecord *rec, ADDRINT regval)
@@ -56,50 +38,51 @@ void on_ins (ADDRINT insaddr, struct insrecord *rec, ADDRINT regval)
 	rec->count ++;
 }
 
+inline bool REG_is_integer (REG reg) {
+	return (reg > REG_RBASE && reg < REG_MM_BASE);
+}
+
 void instruction (INS ins, void *v)
 {
 	ADDRINT addr = INS_Address(ins);
+
 	//fprintf(log, "%x\n", addr);
+	if (insmap.find(addr) != insmap.end())
+		return;
 
-	if (imgloaded && addr >= imglow && addr <= imghigh) {
-		//fprintf(log, "%x\n", addr);
-		if (insmap.find(addr) == insmap.end()) {
-			struct insrecord record;
-			record.opcode = INS_Disassemble(ins);
-			record.count = 0;
-			record.low = record.high = 0;
+	struct insrecord record;
+	record.opcode = INS_Disassemble(ins);
+	record.count = 0;
+	record.low = record.high = 0;
 
-			if (INS_IsBranchOrCall(ins)) {
-				record.reg = MYREG_JMPTARGET;
-			} else {
-				record.reg = MYREG_INVALID;
-				for (UINT32 regindex = 0; regindex < INS_OperandCount(ins); regindex ++) {
-					if (INS_OperandRead(ins, regindex) && INS_OperandIsReg(ins, regindex)) {
-						record.reg = INS_OperandReg(ins, regindex);
-						break;
-					}
-				}
-				if (record.reg == MYREG_INVALID && INS_MaxNumRRegs(ins) > 0)
-					record.reg = INS_RegR(ins, 0);
+	if (INS_IsBranchOrCall(ins)) {
+		record.reg = MYREG_JMPTARGET;
+	} else {
+		record.reg = MYREG_INVALID;
+		for (UINT32 regindex = 0; regindex < INS_OperandCount(ins); regindex ++) {
+			if (INS_OperandRead(ins, regindex) && INS_OperandIsReg(ins, regindex) && REG_is_integer(INS_OperandReg(ins, regindex))) {
+				record.reg = INS_OperandReg(ins, regindex);
+				break;
 			}
-
-			//insmap.insert(addr, record);
-			insmap[addr] = record;
-
-			if (record.reg == MYREG_JMPTARGET) {
-				INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(on_ins), IARG_INST_PTR,
-						IARG_ADDRINT, &insmap[addr], // we assume std::map doesn't move our data.
-						IARG_BRANCH_TARGET_ADDR,
-						IARG_END);
-			} else {
-				INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(on_ins), IARG_INST_PTR,
-						IARG_ADDRINT, &insmap[addr], // we assume std::map doesn't move our data.
-						record.reg == MYREG_INVALID ? IARG_ADDRINT : IARG_REG_VALUE,
-						record.reg == MYREG_INVALID ? 0 : record.reg,
-						IARG_END);
-			}
-
 		}
+		if (record.reg == MYREG_INVALID && INS_MaxNumRRegs(ins) > 0 && REG_is_integer(INS_RegR(ins, 0)))
+			record.reg = INS_RegR(ins, 0);
+	}
+
+	//insmap.insert(addr, record);
+	insmap[addr] = record;
+
+	if (record.reg == MYREG_JMPTARGET) {
+		INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(on_ins), IARG_INST_PTR,
+				IARG_ADDRINT, &insmap[addr], // we assume std::map doesn't move our data.
+				IARG_BRANCH_TARGET_ADDR,
+				IARG_END);
+	} else {
+		INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(on_ins), IARG_INST_PTR,
+				IARG_ADDRINT, &insmap[addr], // we assume std::map doesn't move our data.
+				record.reg == MYREG_INVALID ? IARG_ADDRINT : IARG_REG_VALUE,
+				record.reg == MYREG_INVALID ? 0 : record.reg,
+				IARG_END);
 	}
 }
 
@@ -129,11 +112,10 @@ int main (int argc, char *argv[])
 
 	log = fopen(logname, "w");
 
-	PIN_InitSymbols();
+	//PIN_InitSymbols();
 
 	PIN_AddFiniFunction(on_fini, 0);
 	IMG_AddInstrumentFunction(img_load, NULL);
-	IMG_AddUnloadFunction(img_unload, NULL);
 	INS_AddInstrumentFunction(instruction, NULL);
 
 	PIN_StartProgram(); // Never returns
