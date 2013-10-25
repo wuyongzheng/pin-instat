1a53b898a09afa04278aaa723e523de13712bc86
Wu Yongzheng <wuyongzheng@gmail.com>
2013-10-25 17:05:20 +0800

    bug in INS_InsertCall mutiple times

--- instat.cpp
+++ instat.cpp
@@ -10,6 +10,7 @@
 #define MYREG_INVALID   ((REG)(REG_LAST + 1))
 #define MYREG_JMPTARGET ((REG)(REG_LAST + 2))
 struct insrecord {
+	//ADDRINT addr;
 	string opcode;
 	REG reg;
 	int count;
@@ -26,6 +27,7 @@ std::map<ADDRINT,insrecord> insmap;
 std::map<ADDRINT,string> symbols;
 std::map<ADDRINT,std::pair<ADDRINT,string> > imgs;
 std::set<ADDRINT> calltargets;
+bool ins_conflict_detected = false;
 
 void img_load (IMG img, void *v)
 {
@@ -50,13 +52,6 @@ void img_load (IMG img, void *v)
 			calltargets.insert(RTN_Address(rtn));
 		}
 	}
-
-/*	for (SYM sym = IMG_RegsymHead(img); SYM_Valid(sym); sym = SYM_Next(sym)) {
-		ADDRINT addr = IMG_StartAddress(img) + SYM_Value(sym);
-		fprintf(logfp, "%08x %s\n", addr, SYM_Name(sym).c_str());
-		symbols[addr] = SYM_Name(sym);
-		calltargets.insert(addr);
-	}*/
 }
 
 void on_branch_taken (struct insrecord *rec)
@@ -81,55 +76,68 @@ void instruction (INS ins, void *v)
 {
 	ADDRINT addr = INS_Address(ins);
 
-	if (insmap.find(addr) != insmap.end())
-		return;
-
-	struct insrecord record;
-	record.opcode = INS_Disassemble(ins);
-	record.count = 0;
-	record.branch_taken = -1;
-	record.low = -1;
-	record.high = 0;
-	record.iscall = INS_IsCall(ins);
-
-	if (INS_IsBranchOrCall(ins)) {
-		record.reg = MYREG_JMPTARGET;
-		if (INS_IsDirectCall(ins)) {
-			calltargets.insert(INS_DirectBranchOrCallTargetAddress(ins));
-			record.low = record.high = INS_DirectBranchOrCallTargetAddress(ins);
-		}
-	} else {
-		record.reg = MYREG_INVALID;
-		for (UINT32 regindex = 0; regindex < INS_OperandCount(ins); regindex ++) {
-			if (INS_OperandRead(ins, regindex) && INS_OperandIsReg(ins, regindex) && REG_is_integer(INS_OperandReg(ins, regindex))) {
-				record.reg = INS_OperandReg(ins, regindex);
-				break;
+	/* PIN may call us mutiple times on the same instruction.
+	 * We need to do INS_InsertCall everytime, but initiallize insrecord for the first time.
+	 * We can't handle the situation when different code are loaded into the same address at different time. */ 
+	bool firsttime = true;
+	if (insmap.find(addr) != insmap.end()) {
+		if (INS_Disassemble(ins) != insmap[addr].opcode) {
+			if (!ins_conflict_detected) {
+				fprintf(logfp, "conflicting instruction at %p. old=\"%s\", new=\"%s\". Statistics is incomplete.\n",
+						(void *)addr, insmap[addr].opcode.c_str(), INS_Disassemble(ins).c_str());
+				ins_conflict_detected = true;
 			}
+			return;
 		}
-		if (record.reg == MYREG_INVALID && INS_MaxNumRRegs(ins) > 0 && REG_is_integer(INS_RegR(ins, 0)))
-			record.reg = INS_RegR(ins, 0);
+		firsttime = false;
 	}
+	struct insrecord &record = insmap[addr];
 
-	insmap[addr] = record;
+	if (firsttime) {
+		//record.addr = addr;
+		record.opcode = INS_Disassemble(ins);
+		record.count = 0;
+		record.branch_taken = INS_IsBranch(ins) && INS_HasFallThrough(ins) ? 0 : -1;
+		record.low = -1;
+		record.high = 0;
+		record.iscall = INS_IsCall(ins);
+
+		if (INS_IsBranchOrCall(ins)) {
+			record.reg = MYREG_JMPTARGET;
+			if (INS_IsDirectCall(ins)) {
+				calltargets.insert(INS_DirectBranchOrCallTargetAddress(ins));
+				record.low = record.high = INS_DirectBranchOrCallTargetAddress(ins);
+			}
+		} else {
+			record.reg = MYREG_INVALID;
+			for (UINT32 regindex = 0; regindex < INS_OperandCount(ins); regindex ++) {
+				if (INS_OperandRead(ins, regindex) && INS_OperandIsReg(ins, regindex) && REG_is_integer(INS_OperandReg(ins, regindex))) {
+					record.reg = INS_OperandReg(ins, regindex);
+					break;
+				}
+			}
+			if (record.reg == MYREG_INVALID && INS_MaxNumRRegs(ins) > 0 && REG_is_integer(INS_RegR(ins, 0)))
+				record.reg = INS_RegR(ins, 0);
+		}
+	}
 
 	if (record.reg == MYREG_JMPTARGET) {
 		INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(on_ins),
-				IARG_ADDRINT, &insmap[addr], // we assume std::map doesn't move our data.
+				IARG_ADDRINT, &record, // we assume std::map doesn't move our data.
 				IARG_BRANCH_TARGET_ADDR,
 				IARG_BOOL, INS_IsCall(ins) && (!INS_IsDirectCall(ins)),
 				IARG_END);
 	} else {
 		INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(on_ins),
-				IARG_ADDRINT, &insmap[addr],
+				IARG_ADDRINT, &record,
 				record.reg == MYREG_INVALID ? IARG_ADDRINT : IARG_REG_VALUE,
 				record.reg == MYREG_INVALID ? 0 : record.reg,
 				IARG_BOOL, false,
 				IARG_END);
 	}
 	if (INS_IsBranch(ins) && INS_HasFallThrough(ins)) {
-		insmap[addr].branch_taken = 0;
 		INS_InsertCall(ins, IPOINT_TAKEN_BRANCH, AFUNPTR(on_branch_taken),
-				IARG_ADDRINT, &insmap[addr],
+				IARG_ADDRINT, &record,
 				IARG_END);
 	}
 }
